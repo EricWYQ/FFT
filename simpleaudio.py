3e22ee4beebafacf3599af46aa5b762fee331ffc
EricWYQ <90267793+EricWYQ@users.noreply.github.com>
2021-12-11 22:42:46 -0500

    Update simpleaudio.py

--- simpleaudio.py
+++ simpleaudio.py
@@ -8,15 +8,12 @@ from time import sleep
 
 import pylab as pl
 
-# seed the random number generator
 random.seed()
 
-# Some default values for the audio format
 CHUNK = 256
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 48000
-# This is needed for rescaling
 MAX_AMP = 2**15 - 1
 
 
@@ -36,40 +33,31 @@ class Audio(pyaudio.PyAudio):
         self.format = format
         self.nptype = self.get_np_type(format)
 
-        # Set the curent data to an empty array of the correct type
         self.data = np.array([], dtype=self.nptype)
-
-        # No streams are open at the moment
+        
         self.istream = None
         self.ostream = None
 
-        # a counter for referencing the data in chunks
         self.chunk_index = 0
 
     def __del__(self):
         self.terminate()
 
-    # Get a chunk of data from the current input stream
     def get_chunk(self):
         tmpstr = self.istream.read(self.chunk)
         array = np.fromstring(tmpstr, dtype=self.nptype)
         self.data = np.append(self.data, array)
-    
-    # Put a chunk of data to the current output stream        
+        
     def put_chunk(self):
         slice_from = self.chunk_index*self.chunk
         slice_to = min(self.data.shape[0], slice_from + self.chunk)
-        # Slicing a numpy array out of bounds doesn't seem to raise an
-        # index error, so we explicitly test and raise the error ourselves
+
         if slice_from > self.data.shape[0]:
             raise IndexError
         array = self.data[slice_from:slice_to]
         self.ostream.write(array.tostring())
         self.chunk_index += 1
         
-    # Open an input stream
-    # We just call the inherited open function in the parent class 
-    # with the correct format data
     def open_input_stream(self):
         self.istream = self.open(format=self.format,
                                  channels=self.chan,
@@ -77,25 +65,21 @@ class Audio(pyaudio.PyAudio):
                                  input=True,
                                  frames_per_buffer=self.chunk)
    
-    # Close the input stream
     def close_input_stream(self):
         self.istream.close()
         self.istream = None
       
-    # Open an output stream
     def open_output_stream(self):
         self.ostream = self.open(format=self.format,
                                  channels=self.chan,
                                  rate=self.rate,
                                  output=True)
         self.chunk_index = 0
-             
-    # close the output stream                   
+                
     def close_output_stream(self):
         self.ostream.close()
         self.ostream = None
-    
-    # Record data
+
     def record(self, time=5.0):
         # Clear current data
         self.data = np.array([], dtype=self.nptype)
@@ -109,115 +93,71 @@ class Audio(pyaudio.PyAudio):
         # Close the input stream
         self.close_input_stream()
 
-    # Play the current data
     def play(self):
-        # Open an outputstream
         self.open_output_stream()
-        # Reset the chunk counter to 0
         self.chunk_index = 0
         print("Playing...")
-        # Loop (potentially forever)
         while True:
-            # Try to put output a chunk
             try:    
                 self.put_chunk()
-            # If we run out of data to output, break out of the loop
             except IndexError:
                 break
         
         sleep(0.4) # hack to work around a bug in some (non-blocking) audio hardware 
-        print("Stopped playing")
         # Close the output stream
         self.close_output_stream()
 
-    # Save the data to a file
     def save(self, path):
-        # Create a 'string' of the data
         raw = self.data.tostring()
-        # Open the file for writing
         wf = wave.open(path, 'wb')
-        # Set the header information
         wf.setnchannels(self.chan)
         wf.setsampwidth(self.get_sample_size(self.format))
         wf.setframerate(self.rate)
-        # Write the data
         wf.writeframes(raw)
-        # Close the file
         wf.close()
-    
-    # Load data from a file    
+
     def load(self, path):
-        # Open the file for reading
         wf = wave.open(path, "rb")
-        # Get information from the files header
         self.format = self.get_format_from_width(wf.getsampwidth())
         self.nptype = self.get_np_type(self.format)
         self.chan = wf.getnchannels()
         self.rate = wf.getframerate()
-        # Set the internal data attribute to an empty array of the right type
         self.data = np.array([], dtype=self.nptype)
-        # Read a chunk of data from the file
         raw = wf.readframes(self.chunk)
-        # Loop while there is data in the file
         while raw:
-            # Convert the raw data to a numpy array
             array = np.fromstring(raw, dtype=self.nptype)
-            # Append the array to the class data attribute
             self.data = np.append(self.data, array)
-            # Read the next chunk, ready for the next loop iteration
             raw = wf.readframes(self.chunk)
-        # Close the file
         wf.close()
     
-    # Convert the pyaudio data format type to the numpy type 
-    #  - This really needs expanding to deal with other data types, e.g. 8bit and 24bit audio
     def get_np_type(self, type):
         if type == pyaudio.paInt16:
             return np.int16
     
-    # Convert the numpy data format type to the pyaudio type    
     def get_pa_type(self, type):
         if type == np.int16:
             return pyaudio.paInt16
     
-    # Add an echo the the current audio data
-    #   repeat - How many delayed repeats to add
-    #   delay  - How long to delay each repeat (in samples)
     def add_echo(self, repeat, delay):
         # get the length of the existing data
         length = self.data.shape[0]
         # create a new array with the required extra length
         array = np.zeros(length + repeat*delay, dtype=np.float)
 
-        # loop for the number of delays + 1
-        #  - we use the 0th iteration of the loop to reduce the amplitude of the original
-        #    waveform, so when we add to it we don't 'clip'
         for i in range(0, repeat+1):
             # Get start and end times for the current offset
             start = i*delay
             end = length + i*delay
-            # Calculate the current scaling factor
             scale = 2**(i+1)
-            # Add a scaled version of self.data to 'window' of the new array
             array[start:end] += self.data / scale
-        # Set the class data attribute to the new array
         self.data = np.rint(array).astype(self.nptype)
 
     def rescale(self, val):
-        # Check argument passed
         if not 0 <= val <= 1:
             raise ValueError("Expected scaling factor between 0 and 1")
-
-        # find the biggest peak
-        # peak = 0
-        # length = self.data.shape[0]
-        # for i in range(0, length-1):
-        #     if abs(self.data[i]) > peak:
-        #         peak = abs(self.data[i])
-
+            
         peak = np.max(np.abs(self.data))
 
-        # Calculate the rescaling factor
         rescale_factor = val*MAX_AMP/peak
 
         self.data = (self.data * rescale_factor).astype(self.nptype)
@@ -225,16 +165,13 @@ class Audio(pyaudio.PyAudio):
     def create_tone(self, frequency, length, amplitude):
         if not 0 <= amplitude <= 1:
             raise ValueError("Expected amplitude between 0 and 1")
-
-        # create new array of requested length
+            
         s = np.zeros(length, self.nptype)
 
-        # iterate through array, creating waveform sample by sample
         for i in range(0, length):
             s[i] = amplitude * MAX_AMP \
                    * math.sin(frequency * i * 2 * math.pi/self.rate)
 
-        # set instance data to the newly created array
         self.data = s
 
     def create_noise(self, length, amplitude):
@@ -242,26 +179,19 @@ class Audio(pyaudio.PyAudio):
         if not 0 <= amplitude <= 1:
             raise ValueError("Expected amplitude between 0 and 1")
 
-        # create new array of requested length
         s = np.zeros(length, self.nptype)
 
-        # iterate through array, creating waveform sample by sample
         for i in range(0, length):
             s[i] = amplitude * MAX_AMP * random.random()
 
-        # set instance data to the newly created array
         self.data = s
 
 
     def add(self, other):
-        # Find the length of the longest
         length = max(self.data.shape[0], other.data.shape[0])
-        # Create an empty array of this length
         array = np.zeros(length, dtype=self.nptype)
-        # Add in each data at half amplitute (so it doesn't clip)
         array += self.data / 2.0
         array += other.data / 2.0
-        # Update the stored array in the current object.
         self.data = array
 
     def __len__(self):
@@ -327,7 +257,6 @@ class Audio(pyaudio.PyAudio):
     def plot_spectrum(self, array, start=0, end=-1, plot_log=False):
         array = array[start:end]
         len_arr = len(array)
-        # print(len_arr)
         freq_axis = np.arange(0, len_arr, 1.0)  # * (self.rate / len_arr)
         if plot_log:
             pl.plot(freq_axis/1000, 10*np.log10(array), color='k')
@@ -338,25 +267,15 @@ class Audio(pyaudio.PyAudio):
         pl.show()
 
 
-# This version uses a function just defined in the module namespace (i.e. not a method of the class),
-# and takes one argument that is a list of audio objects. This allows an arbitrary number of objects and uniform scaling
 def sum_audio(audio_objects):
-    # Get the length of the longest.
-    #   - the max() function when given an iterable object will give the max in that 'list', 
-    #   - you can also specifiy a function to use to evaluate the size of each object using key=function_name
     length = len(max(audio_objects, key=len))
-    # Work out the required scaling factor to prevent clipping
     scale = 1.0/len(audio_objects)
 
-    # make an array of zeros
-    # - should really check that the dtype of each of the objects is the same and use that dtype!
     array = np.zeros(length, dtype=np.int16)
     
-    # Add each audio_object to the array
     for obj in audio_objects:
         array += np.rint(obj.data * scale).astype(np.int16)
     
-    # Create a new object to return
     new_object = Audio()
     new_object.data = array
     
